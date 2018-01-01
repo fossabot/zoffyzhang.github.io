7171c551868ff162f116d696e9bd9cdac79c21db
Zoffy Zhang <zoffyzhang@foxmail.com>
2018-01-01 23:55:17 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+.page
+    input(type="text", placeholder="test")
+    input(type="text", placeholder="test")
+    input(type="text", placeholder="test")
+    button(@click="log") button
+</template>
+
+<script>
+export default {
+    data() {
+        return {}
+    },
+    methods: {
+        log() {
+            console.log('ssdsdsds')
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+.page {
+    input {
+        background: cyan;
+    }
+}
+</style>
